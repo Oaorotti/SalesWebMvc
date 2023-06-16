52a4334869e15991803bacc7e1557988d156a053
Joao Souza <joaowsouza1337@gmail.com>
2023-06-16 17:21:28 -0300

    added sales classes and migration

---
+++ SalesWebMvc/Models/Seller.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+
+namespace SalesWebMvc.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public double BaseSalary { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Department Department { get; set; }
+
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+        public Seller()
+        {
+
+        }
+
+        public Seller(int id, string name, string email, double baseSalary, DateTime birthDate, Department department)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BaseSalary = baseSalary;
+            BirthDate = birthDate;
+            Department = department;
+        }
+
+        public void AddSales(SalesRecord sales)
+        {
+            Sales.Add(sales);
+        }
+
+        public void RemoveSales(SalesRecord sales)
+        {
+            Sales.Remove(sales);
+        }
+
+        public double TotalSales(DateTime initialDate, DateTime finalDate)
+        {
+            return Sales.Where(salesrecord => salesrecord.Date >= initialDate && salesrecord.Date <= finalDate).Sum(salesrecord => salesrecord.Amount);
+        }
+    }
+}
